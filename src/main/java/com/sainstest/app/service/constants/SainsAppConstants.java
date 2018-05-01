c2cdb4ee068429b7a3eeca52a857c2d7966288ce
Mavrothalassitis <panos.mavrothalassitis@bt.com>
2018-05-01 10:54:39 +0100

    adding constants package with constants used across the app

---
+++ src/main/java/com/sainstest/app/service/constants/SainsAppConstants.java
@@ -0,0 +1,16 @@
+package com.sainstest.app.service.constants;
+
+/**
+ * App constants.
+ */
+public final class SainsAppConstants {
+
+    private SainsAppConstants() {}
+
+    public final static String KCAL = "kcal";
+    public final static String DEFAULT_CURRENCY = "£";
+    public final static String EMPTY_STRING = "";
+    public final static String EMPTY_JSON_VALUE = "{}";
+    public final static String ENERGY_KCAL = "Energy kcal";
+    public final static String DESCRIPTION = "Description";
+}
